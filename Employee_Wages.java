447f803477d58803c82947076fafef8b5fc9e504
biradarajay1904 <biradarajay04@gmail.com>
2022-07-15 16:48:43 +0530

    modified: employee  wages using switch case

--- Employee_Wages.java
+++ Employee_Wages.java
@@ -4,30 +4,30 @@ package com.Employee.wages;
 public class Employee_Wages {
 	public static void main(String args[]) {
 		System.out.println("Welcome to Employee Attendance");
- int dailyWage = 0;
- int noofpartHourperday = 4;
- int noOfhourperday = 8;
- int Wageperhr = 20;
-     int attendence= (int) (Math.floor(Math.random()*10))%2;
-     
-switch (attendence) {
+		 int dailyWage = 0;
+		 int noofpartHourperday = 4;
+		 int noOfhourperday = 8;
+		 int Wageperhr = 20;
+		     int attendence= (int) (Math.floor(Math.random()*10))%2;
+		     
+		switch (attendence) {
 
-case 1:
-	System.out.println("Employee is Working Full Time");
-	dailyWage= Wageperhr * noOfhourperday;
-	System.out.println(" Employee daily wage is :" + dailyWage );
-  break;
-case 0:
-    System.out.println("Employee is working part time");
-    dailyWage= Wageperhr * noofpartHourperday ;
-    System.out.println("Employee daily wage is: "+ dailyWage);
-    break;
+		case 1:
+			System.out.println("Employee is Working Full Time");
+			dailyWage= Wageperhr * noOfhourperday;
+			System.out.println(" Employee daily wage is :" + dailyWage );
+		  break;
+		case 0:
+		    System.out.println("Employee is working part time");
+		    dailyWage= Wageperhr * noofpartHourperday ;
+		    System.out.println("Employee daily wage is: "+ dailyWage);
+		    break;
 
- default :
- System.out.println("Employee is absent");
-    break;
-}
+		 default :
+		 System.out.println("Employee is absent");
+		    break;
+		}
 
 
-}
-}
+		}
+		}
