be70492cc21c1710217786ec99e2ccdec8a4c60f
biradarajay1904 <biradarajay04@gmail.com>
2022-07-15 16:28:39 +0530

    added: welcome to attendance of employee

---
+++ Employee_Wages.java
@@ -0,0 +1,17 @@
+package com.Employee.wages;
+
+
+public class Employee_Wages {
+
+	public static void main(String args[]) {
+		System.out.println("Welcome to Employee Attendance");
+		 
+
+			}
+  
+	}
+
+
+
+
+
