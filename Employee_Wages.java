40ce40245b34609ff81cd958f206e93bd7119627
biradarajay1904 <biradarajay04@gmail.com>
2022-07-15 16:43:18 +0530

    modified: employee daily wages

--- Employee_Wages.java
+++ Employee_Wages.java
@@ -2,25 +2,28 @@ package com.Employee.wages;
 
 
 public class Employee_Wages {
-
-	public static void main(String args[]) {
-		System.out.println("Welcome to Employee Attendance");
-		 int Attendance=1; 
-		  double randomCheck= (Math.floor(Math.random()*10))%2;
-		  if (Attendance==randomCheck) {
-			  System.out.println("Employee is present");
-			  
-		  } else {
-			  System.out.println("Employee is absent");
-		  }
-
-			}
-
-			}
-  
 	
+	public static void main(String args[]) {
+	System.out.println ("Welcome to Employee Attendance");
+	 int noOfhourperday = 8;
+	 int Wageperhr = 20;
+	 
+	  int Attendance=1; 
+	  double randomCheck= (Math.floor(Math.random()*10)) %2;
+	  
+	  if (Attendance == randomCheck) {
+		  
+		  if(noOfhourperday == 8) {
+		  System.out.println("Employee is present");
+		  int totalWage = noOfhourperday * Wageperhr;
+		  System.out.println("wage per day is:" + totalWage);
+		  
+		  }  
+	  } 
+		  
+		   else {
+		  System.out.println(" Employee is Absent");
+	  }
 
-
-
-
-
+		}
+	}
